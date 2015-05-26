4d35156a3199ec48f214e8f1aa55460c2113d36e
Eklavya Sharma <sharmaeklavya2@hotmail.com>
2015-05-26 14:12:02 +0530

    Added media server

--- tango_project/urls.py
+++ tango_project/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
 admin.autodiscover()
 
@@ -11,3 +10,11 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^rango/', include('rango.urls')),
 )
+
+import settings
+
+if settings.DEBUG:
+	urlpatterns+= patterns(
+		'django.views.static',
+		(r'media/(?P<path>.*)','serve',{'document_root':settings.MEDIA_ROOT})
+	)
