570f9325b5807d73ae3f49e6f5b0c4ad1bac85b8
Eklavya Sharma <sharmaeklavya2@hotmail.com>
2015-05-26 16:43:39 +0530

    Added models

--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+	name = models.CharField(max_length=128)
+	
+	def __unicode__(self):
+		return self.name
+
+class Page(models.Model):
+	category = models.ForeignKey(Category)
+	title = models.CharField(max_length=64)
+	views = models.IntegerField(default=0)
+	url = models.URLField()
+	
+	def __unicode__(self):
+		return self.title
+
--- tango_project/settings.py
+++ tango_project/settings.py
@@ -57,7 +57,7 @@ WSGI_APPLICATION = 'tango_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'rango.db'),
     }
 }
 
