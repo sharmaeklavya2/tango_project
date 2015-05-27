9bf0f73943dc4864a82fac0b88d69822f38667d5
Eklavya Sharma <sharmaeklavya2@hotmail.com>
2015-05-27 17:10:20 +0530

    Added a page for each category and linked them to rango index page

--- rango/urls.py
+++ rango/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url, patterns
 import views
 
 urlpatterns = patterns('',
-		url(r'^$',views.index,name='index'),
+		url(r'^$', views.index, name='index'),
+		url(r'^category/(?P<category_enc_name>\w+)/$', views.category, name='category')
 	)
--- rango/views.py
+++ rango/views.py
@@ -1,8 +1,27 @@
+from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from models import Category
+from models import Category, Page
 
 def index(request):
 	context = RequestContext(request)
-	context_dict = {'categories': Category.objects.all()}
+	category_list = Category.objects.all()
+	for cat in category_list:
+		cat.enc_name = cat.name.replace(' ','_')
+	context_dict = {'categories': category_list}
 	return render_to_response('rango/index.html', context_dict, context)
+
+def category(request, category_enc_name):
+	context = RequestContext(request)
+	category_name = category_enc_name.replace('_',' ')
+	context_dict = {}
+	
+	try:
+		cat = Category.objects.get(name=category_name)
+		pages = Page.objects.filter(category=cat)
+		context_dict['pages']=pages
+		context_dict['category']=cat
+	except Category.DoesNotExist:
+		return HttpResponse(category_name + " is an invalid category")
+	
+	return render_to_response("rango/category.html",context_dict,context)
