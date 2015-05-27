5d0ea2c833e616570101fe282a4b4b1b4c0ee277
Eklavya Sharma <sharmaeklavya2@hotmail.com>
2015-05-27 15:06:05 +0530

    Changed model 'Category' and updated population script and data

--- populate_db.py
+++ populate_db.py
@@ -1,5 +1,5 @@
-def add_cat(name):
-	return Category.objects.get_or_create(name=name)[0]
+def add_cat(name,views,likes):
+	return Category.objects.get_or_create(name=name,views=views,likes=likes)[0]
 
 def add_page(cat, title, url, views=0):
 	return Page.objects.get_or_create(category=cat, title=title, url=url, views=views)[0]
@@ -7,15 +7,34 @@ def add_page(cat, title, url, views=0):
 def populate():
 	cat_dict = {}
 	
-	ifile = open("pop_data.txt")
+	ifile = open("rango_pop_data/category.txt")
 	for line in ifile:
-		cat_name, title, url = line.split('\t')
-		if cat_name in cat_dict:
-			cat = cat_dict[cat_name]
+		views=0
+		likes=0
+		words=line.split('\t')
+		if len(words) < 1:
+			continue
 		else:
-			cat = add_cat(cat_name)
-			cat_dict[cat_name] = cat
-		add_page(cat, title, url)
+			cat_name = words[0]
+			if len(words) >=2:
+				views = int(words[1])
+			if len(words) >=3:
+				likes = int(words[2])
+		cat_dict[cat_name] = add_cat(cat_name,views,likes)
+	ifile.close()
+	
+	ifile = open("rango_pop_data/page.txt")
+	for line in ifile:
+		views = 0
+		words = line.split('\t')
+		if len(words) < 3:
+			continue
+		else:
+			cat_name, title, url = words[0:3]
+		if len(words) == 4:
+			views = int(words[3])
+		add_page(cat_dict[cat_name], title, url, views)
+	ifile.close()
 	
 	for c in Category.objects.all():
 		for p in Page.objects.filter(category=c):
