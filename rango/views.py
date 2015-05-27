9cbd4e72b8b6040bf2bd8124089c8489338bc57b
Eklavya Sharma <sharmaeklavya2@hotmail.com>
2015-05-27 15:54:11 +0530

    Rango's index page now shows list of categories from rango.db

--- rango/views.py
+++ rango/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from models import Category
 
 def index(request):
 	context = RequestContext(request)
-	context_dict = {
-			'title':'Rango',
-			'body': 'Get ready to Rango!'
-		}
+	context_dict = {'categories': Category.objects.all()}
 	return render_to_response('rango/index.html', context_dict, context)
