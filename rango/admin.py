ef40aa1b1e194c1083b0c82f24d09e9268ef39e7
Eklavya Sharma <sharmaeklavya2@hotmail.com>
2015-06-13 21:56:01 +0530

    Customized admin interface

--- rango/admin.py
+++ rango/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from models import Category, Page, UserProfile
+from django.contrib.admin import ModelAdmin
 
-admin.site.register(Category)
-admin.site.register(Page)
-admin.site.register(UserProfile)
+class CategoryAdmin(ModelAdmin):
+	list_display=('name','views','likes')
+
+class PageAdmin(ModelAdmin):
+	list_display=('title','category','url','views')
+
+class UserProfileAdmin(ModelAdmin):
+	list_display=('user','website')
+
+admin.site.register(Category,CategoryAdmin)
+admin.site.register(Page,PageAdmin)
+admin.site.register(UserProfile,UserProfileAdmin)
--- rango/models.py
+++ rango/models.py
@@ -8,6 +8,9 @@ class Category(models.Model):
 	
 	def __unicode__(self):
 		return self.name
+	
+	class Meta:
+		verbose_name_plural = "Categories"
 
 class Page(models.Model):
 	category = models.ForeignKey(Category)
